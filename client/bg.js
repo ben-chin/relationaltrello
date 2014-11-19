33d1549c3f1369a7498008dec96d9f88b075a2b8
ben-chin <ben.chin101@gmail.com>
2014-11-19 20:56:29 +0000

    All two-way bindings working except subcard creation and subboard/list deletion, using outgoing requests method - major refactor needed!

--- client/bg.js
+++ client/bg.js
@@ -1,9 +1,16 @@
 App = new App();
 
+// Send message to current tab
+var sendMsg = function (message) {
+	chrome.tabs.query({ active: true, currentWindow: true }, 
+		function (tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, message);
+		});
+};
 
 // Extract board id and card id from the url of 
 // the request sent from opening a card
-var extractDetails = function (url) {
+var extractCardDetails = function (url) {
 	var pattern = /https:\/\/trello.com\/1\/boards\/([a-zA-Z0-9]*)\/cards\/([a-zA-Z0-9]*)/gm;
 	var matches = pattern.exec(url);
 	return {
@@ -12,25 +19,264 @@ var extractDetails = function (url) {
 	};
 };
 
-// Listen for the request that gets sent when opening a card
+var extractCheckItemCreatedDetails = function (url) {
+	var pattern = /https:\/\/trello.com\/1\/cards\/([a-zA-Z0-9]*)\/checklist\/([a-zA-Z0-9]*)\/checkItem/gm;
+	var matches = pattern.exec(url);
+	return {
+		cardId: matches[1],
+		checklistId: matches[2]
+	};
+};
+
+var extractCheckItemUpdatedDetails = function (url) {
+	var pattern = /https:\/\/trello.com\/1\/cards\/([a-zA-Z0-9]*)\/checklist\/([a-zA-Z0-9]*)\/checkItem\/([a-zA-Z0-9]*)/gm;
+	var matches = pattern.exec(url);
+	return {
+		cardId: matches[1],
+		checklistId: matches[2],
+		checkItemId: matches[3]
+	};
+};
+
+var extractCheckItemDeletedDetails = function (url) {
+	var pattern = /https:\/\/trello.com\/1\/cards\/([a-zA-Z0-9]*)\/checklist\/([a-zA-Z0-9]*)\/checkItem\/([a-zA-Z0-9]*)/gm;
+	var matches = pattern.exec(url);
+	return {
+		cardId: matches[1],
+		checklistId: matches[2],
+		checkItemId: matches[3]
+	};
+};
+
+var extractSubcardDetails = function (url) {
+	var pattern = /https:\/\/trello.com\/1\/cards\/([a-zA-Z0-9]*)/gm;
+	var matches = pattern.exec(url);
+	return {
+		cardId: matches[1],
+	};
+};
+
+
+
+var isCardOpenedRequest = function (request) {
+	var pattern = /https:\/\/trello.com\/1\/boards\/([a-zA-Z0-9]*)\/cards\/([a-zA-Z0-9]*)/gm;
+	return pattern.test(request.url) && request.method == "GET";
+};
+
+// Checkitem CRUD (two-way mirrorring pt I)
+var isCheckItemCreatedRequest = function (request) {
+	var pattern = /https:\/\/trello.com\/1\/cards\/([a-zA-Z0-9]*)\/checklist\/([a-zA-Z0-9]*)\/checkItem/gm;
+	return pattern.test(request.url) && request.method == "POST";
+
+};
+
+var isCheckItemUpdatedRequest = function (request) {
+	var pattern = /https:\/\/trello.com\/1\/cards\/([a-zA-Z0-9]*)\/checklist\/([a-zA-Z0-9]*)\/checkItem\/([a-zA-Z0-9]*)/gm;
+	return pattern.test(request.url) && request.method == "PUT";
+};
+
+var isCheckItemDeletedRequest = function (request) {
+	var pattern = /https:\/\/trello.com\/1\/cards\/([a-zA-Z0-9]*)\/checklist\/([a-zA-Z0-9]*)\/checkItem\/([a-zA-Z0-9]*)/gm;
+	return pattern.test(request.url) && request.method == "DELETE";
+};
+
+// TODO: mark checkitem as done
+
+// Subcard CRUD (two-way mirrorring pt II)
+//   - card creation ?!?!?
+//   - card deletion (have to handle update to CLOSED == archived card)
+var isCardUpdatedRequest = function (request) {
+	var pattern = /https:\/\/trello.com\/1\/cards\/([a-zA-Z0-9]*)/gm;
+	return pattern.test(request.url) && request.method == "PUT";
+};
+
+// TODO: checklist deletion -> subboard deletion
+// TODO: subboard deletion -> checklist deletion
+
+// Listen for the requests that tell us what the user is doing
 chrome.webRequest.onCompleted.addListener(
-	function (details) {
-		if (details.statusCode !== 200)	return;
+	function (request) {
+		if (request.statusCode !== 200)	return;
 
-		var message = {
-			name: 'cardOpened',
-			data: extractDetails(details.url)
-		};
+		console.log(request);
 
-		chrome.tabs.query({	active: true, currentWindow: true }, 
-			function (tabs) {
-				console.log(message);
-  				chrome.tabs.sendMessage(tabs[0].id, message);
-  			}
-  		);
+		// A card has been opened
+		if (isCardOpenedRequest(request)) {
+			var message = {};
+			message.name = 'cardOpened';
+
+			var data = extractCardDetails(request.url);
+			Trello.get('/cards/' + data.cardId + '/checklists/').then(function (checklists) {
+				async.map(checklists, 
+					function iterator (checklist, callback) {
+				        App.isSubboardChecklist(checklist.id)
+				        	.then(function (result) {
+				        		if(!$.isEmptyObject(result)) {
+				        			callback(null, true);
+				        		} else {
+				        			callback(null, false);
+				        		}
+				        	})
+				        	.fail(function (err) {
+				        		callback(err, null);
+				        	});
+				    }, function success (err, results) {
+				        if (err) {
+				        	console.log('oops');
+				        } else {
+				        	data.hasSubboard = results.length > 0 ? results.reduce(function(a, b) { return a && b; }) : false;
+				        	message.data = data;
+				        	sendMsg(message);
+				        }
+				    });
+			});
+		} 
+
+		// A checkitem has been created
+		else if (isCheckItemCreatedRequest(request)) {
+			var data = extractCheckItemCreatedDetails(request.url);
+
+			chrome.storage.local.get(data.checklistId, function (result) {
+				if (chrome.runtime.lastError) {
+					console.log('Not here!');
+				} else {
+					if (!$.isEmptyObject(result)) {
+						App.createNewTask(data.checklistId, result[data.checklistId]);
+					} else {
+						console.log('Not a subboard checklist!');
+					}
+				}
+			});
+		}
+
+		// A checkitem has been updated
+		else if (isCheckItemUpdatedRequest(request)) {
+			var data = extractCheckItemUpdatedDetails(request.url);
+			var endpoint = '/checklist/' + data.checklistId + '/checkItems/' + data.checkItemId;
+
+			App.getFromStorage(data.checkItemId).then(function (result) {
+				if (!$.isEmptyObject(result)) {
+					var subCardId = result[data.checkItemId];
+
+					Trello.get(endpoint).then(function (checkItem) {
+						var newCardData = {
+							name: checkItem.name
+						};
+
+						App.getFromStorage(data.checklistId).then(function (result) {
+							var subboardId = result[data.checklistId];
+							Trello.get('/boards/' + subboardId + '/lists/')
+								.then(function (lists) {
+									var lastListId = lists[lists.length - 1].id;
+									var firstListId = lists[0].id;
+
+									// checkitem ticked
+									if (checkItem.state == 'complete') {
+										newCardData.idList = lastListId;
+
+										Trello.put('/cards/' + subCardId, newCardData).then(function (data) {
+											// TODO: Set success message for content script
+											console.debug('successfully updated subcard name and position', data);
+										});
+									// checkitem unticked
+									} else {
+										Trello.get('/cards/' + subCardId).then(function (card) {
+											// card was previously in done (but corresponding checkitem is now unticked)
+											if (card.idList == lastListId) {
+												newCardData.idList = firstListId;
+											}
+
+											Trello.put('/cards/' + subCardId, newCardData).then(function (data) {
+												// TODO: Set success message for content script
+												console.debug('successfully updated subcard name and position', data);
+											});
+										});
+									}
+								});
+						});
+					});
+				} else {
+					console.log('not a checkitem for a subboard');
+				}
+			});
+		}
+
+		// A checkitem has been deleted
+		else if (isCheckItemDeletedRequest(request)) {
+			var data = extractCheckItemDeletedDetails(request.url);
+			var endpoint = '/checklist/' + data.checklistId + '/checkItems/' + data.checkItemId;
+
+			App.getFromStorage(data.checkItemId).then(function (result) {
+				if (!$.isEmptyObject(result)) {
+					var subCardId = result[data.checkItemId];
+
+					Trello.delete('/cards/' + subCardId).then(function () {
+						// TODO: Set success message for content script
+						console.debug('successfully deleted subcard');
+						// Delete mapping
+						chrome.storage.local.remove(subCardId);
+						chrome.storage.local.remove(data.checkItemId);
+					});
+				} else {
+					console.log('not a checkitem for a subboard');
+				}
+			});
+		}
+
+		// A card has been updated
+		else if (isCardUpdatedRequest(request)) {
+			var data = extractSubcardDetails(request.url);
+			console.log(data);
+			App.getFromStorage(data.cardId).then(function (result) {
+				if (!$.isEmptyObject(result)) {
+					var checkItemId = result[data.cardId];
+
+					Trello.get('/cards/' + data.cardId).then(function (card) {
+						Trello.get('/boards/' + card.idBoard + '/lists/').then(function (lists) {
+							
+							App.getFromStorage(card.idBoard).then(function (result) {
+								var checklistId = result[card.idBoard];
+
+								Trello.get('/checklist/' + checklistId).then(function (checklist) {
+									var checkItemEndpoint = '/cards/' + checklist.idCard + '/checklist/' + checklistId + '/checkItem/' + checkItemId;
+									var lastListId = lists[lists.length - 1].id;
+									var newCheckItemData = {
+										idChecklistCurrent: checklistId,
+										idCheckItem: checkItemId,
+										value: false
+									};
+
+									if (lastListId == card.idList) {
+										newCheckItemData.value = true;
+									}
+
+									Trello.put(checkItemEndpoint + '/state', newCheckItemData).then(function (data) {
+										// TODO: Set success message for content script
+										console.debug('successfully updated corresponding checkitem state', data);
+										
+										// Update name.
+										newCheckItemData.value = card.name;
+										Trello.put(checkItemEndpoint + '/name', newCheckItemData).then(function (data) {
+											// TODO: Set success message for content script
+											console.debug('successfully updated corresponding checkitem name', data);
+										});
+									});
+								});
+							});
+						});
+
+					});
+				}
+			});
+		}
+
+
 	}, {
 		urls: [
-			"*://trello.com/1/boards/*/cards/*"
+			"*://trello.com/1/boards/*/cards/*",
+			"*://trello.com/1/cards/*",
+			"*://trello.com/1/cards/*/checklist/*/checkItem",
+			"*://trello.com/1/cards/*/checklist/*/checkItem/*"
 		]
 	}
 );
@@ -44,6 +290,47 @@ chrome.runtime.onMessage.addListener(
     	var cardId = message.data.cardId;
     	var cardTitle = message.data.cardTitle;
     	App.createSubboard(cardId, cardTitle);
+    }
+    // Content script has requested the subboard checklists for a board
+    else if (message.name == 'requestSubboardChecklists') {
+    	var subboardChecklistsLinks = [];
+    	var boardlink = message.data.boardlink;
+
+    	// Horrendous recursive loop thanks to async
+ 		var loopAsync = function (checklists, i) {
+ 			if (i < checklists.length) {
+	 			var checklistId = checklists[i].id;
+	 			App.isSubboardChecklist(checklistId)
+					// success!
+	 				.then(function (result) {
+	 					if ($.isEmptyObject(result)) {
+ 							loopAsync(checklists, i + 1);
+	 					} else {
+		 					Trello.get('/checklists/' + checklistId + '/cards/',
+		 						function (cards) {
+		 							var card = cards[0];
+		 							subboardChecklistsLinks.push(card.shortLink);
+
+		 							loopAsync(checklists, i + 1);
+		 						});
+		 				}
+	 				});
+			} else {
+				// Send back valid subboard shortlinks
+		    	sendResponse({
+		    		data: {
+		    			subboardChecklistsLinks: subboardChecklistsLinks
+		    		}
+		    	});
+			}
+		};
+
+    	Trello.get('/board/' + boardlink + '/checklists')
+    		.then(function (checklists) {
+    	 		loopAsync(checklists, 0);
+    	 	});
+
+    	return true;
     }
   
   });
--- client/client.js
+++ client/client.js
@@ -1,9 +1,23 @@
+// Hack the board short link out of the url 
+// for use in the Trello API
+var extractBoardShortlink = function(url) {
+	var pattern = /https:\/\/trello.com\/b\/([a-zA-Z0-9]*)\/*/gm;
+	var matches = pattern.exec(url);
+	return matches ? matches[1] : null;
+};
+
 // Use the card id to render a button in the DOM 
 // that will create a board
 var renderAddButton = function (cardId) {
-	var tpl = '<a href="#" class="button-link subboard" data-card-id="' + cardId + '" title="Create a child board from a checklist.">  SubBoard It </a>';
+	var tpl = '<a href="#" class="button-link create-subboard" data-card-id="' + cardId + '" title="Create a child board from a checklist.">  SubBoard It </a>';
 	$('.window .window-sidebar .window-module:not(.other-actions) .button-link').last().after(tpl);
 };
+
+
+// var renderGotoButton = function (boardId) {
+// 	var tpl = '<a href="#" class="button-link goto-subboard" data-board-id="' + boardId + '" title="Create a child board from a checklist.">  Go To SubBoard </a>';
+// 	$('.window .window-sidebar .window-module:not(.other-actions) .button-link').last().after(tpl);
+// };
 
 // Send a message to the background script to 
 // create a checklist for this card id and 
@@ -17,10 +31,36 @@ var requestSubboard = function (cardId, cardTitle) {
 		}
 	};
 
-	chrome.runtime.sendMessage(message, function (response) {
-		console.log('Yay');
-		console.log(response);
-	});
+	chrome.runtime.sendMessage(message, 
+		function (response) {
+			console.log(response);
+		}
+	);
+};
+
+// Send a message to bg script to
+// scan the board's checklists and return all
+// of the valid subboard checklists on this board
+var requestSubboardChecklists = function (boardlink) {
+	var message = {
+		name: 'requestSubboardChecklists',
+		data: {
+			boardlink: boardlink
+		}
+	};
+	chrome.runtime.sendMessage(message,
+		function (response) {
+			console.log(response);
+			if (response.data){
+				var links = response.data.subboardChecklistsLinks;
+				for (var i = 0; i < links.length; i++) {
+					var link = links[i];
+					var tpl = '<div title="This card has a child board." class="badge badge-state-image-only"><span class="badge-icon icon-sm icon-subboard"></span>  </div>';
+				    $('.list-card-title[href*="' + link + '"]').nextAll('.badges').append(tpl);
+				}
+			}
+		}
+	);
 };
 
 // When the background script picks up an opened card request,
@@ -29,16 +69,31 @@ chrome.runtime.onMessage.addListener(
   function(message, sender, sendResponse) {
     if (message.name == 'cardOpened') {
     	var cardId = message.data.cardId;
-    	renderAddButton(cardId);
+    	var boardId = message.data.boardId;
+
+    	if (message.data.hasSubboard) {
+    		// renderGotoButton(boardId);
+    	} else {
+    		renderAddButton(cardId);
+    	}
     }
   });
 
 
 $(document).ready(function () {
-	$('.window').on('click', '.button-link.subboard', function (e) {
-		requestSubboard(
-			$(this).data('cardId'), 
-			$('.window .window-title .window-title-text').text()
-		);
-	});
+
+	var bsl = extractBoardShortlink(window.location.href);
+	requestSubboardChecklists(bsl);
+
+	$('.window')
+		.on('click', '.button-link.create-subboard', function (e) {
+			requestSubboard(
+				$(this).data('cardId'), 
+				$('.window .window-title .window-title-text').text()
+			);
+		})
+		// .on('click', '.button-link.goto-subboard', function (e) {
+		// 	// Need shortlink. bugger
+		// 	window.location.pathname = '/b/' + $(this).data('boardId');
+		// });
 });
